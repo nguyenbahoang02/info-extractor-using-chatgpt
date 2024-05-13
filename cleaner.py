ae4639c8683ba3eb4b812961186707f63b9f420a
hoang <nguyenbahoang02>
2024-05-13 22:35:36 +0700

    update

--- cleaner.py
+++ cleaner.py
@@ -7,19 +7,19 @@ def read_json_objects_from_file(file_path):
         data = json.load(file)
     return data
 
-file_path = 'relation/final.json'
+file_path = 'title/final.json'
 json_objects = read_json_objects_from_file(file_path)
 
 cleaned_data = []
 
 for obj in json_objects:
-    if 'liên hệ' not in obj:
+    if 'chức vụ' not in obj:
         continue
-    if obj['liên hệ'] == None:
+    if obj['chức vụ'] == None:
         continue
     cleaned_data.append(obj)
 
 
-output_file = 'relation/cleaned_data_1.json'
+output_file = 'title/cleaned_data_1.json'
 with open(output_file, 'w', encoding='utf-8') as file:
     json.dump(cleaned_data, file, indent=4, ensure_ascii=False)
