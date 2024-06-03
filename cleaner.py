424bbdbeb307d4daa750b2015ebe00ea2d7e68e1
hoang <nguyenbahoang02>
2024-06-03 12:39:51 +0700

    clean festivals

--- cleaner.py
+++ cleaner.py
@@ -7,19 +7,24 @@ def read_json_objects_from_file(file_path):
         data = json.load(file)
     return data
 
-file_path = 'title/final.json'
+file_path = 'festival_with_raw_time/final.json'
 json_objects = read_json_objects_from_file(file_path)
+festivals = read_json_objects_from_file("refinedFestivalFromLehoiInfo_2.json")
 
 cleaned_data = []
 
+def check_exist(name):
+    for obj in festivals:
+        if obj["name"] == name:
+            print(name)
+            return True
+    return False
+
 for obj in json_objects:
-    if 'chức vụ' not in obj:
-        continue
-    if obj['chức vụ'] == None:
-        continue
-    cleaned_data.append(obj)
+    if check_exist(obj["name"]):
+        cleaned_data.append(obj)
 
 
-output_file = 'title/cleaned_data_1.json'
+output_file = 'festival_with_raw_time/cleaned_data.json'
 with open(output_file, 'w', encoding='utf-8') as file:
     json.dump(cleaned_data, file, indent=4, ensure_ascii=False)
--- merger.py
+++ merger.py
@@ -7,9 +7,10 @@ def read_json_objects_from_file(file_path):
     return data
 
 
-title_with_description = read_json_objects_from_file(
-    "festival_with_raw_time/extracted_using_chatgpt_37.json")
-titles = read_json_objects_from_file("festival_with_raw_time/final.json")
+historical_figures_with_only_relation = read_json_objects_from_file(
+    "relation/cleaned_data.json")
+historical_figures = read_json_objects_from_file(
+    "HFFromWikipediaWithRelation_2.json")
 
 
 # def check_exist(title_name):
@@ -20,9 +21,16 @@ titles = read_json_objects_from_file("festival_with_raw_time/final.json")
 #     return False
 
 
-for title in title_with_description:
-    titles.append(title)
+for historical_figure in historical_figures_with_only_relation:
+    for hf in historical_figures:
+        if hf["name"] == historical_figure["name"]:
+            for relation in historical_figure["liên hệ"]:
+                if relation["loại"] not in hf:
+                    hf[relation["loại"]] = []
+                if not isinstance(hf[relation["loại"]], list):
+                    hf[relation["loại"]] = []
+                hf[relation["loại"]].append(relation["tên"])
 
-output_file = 'festival_with_raw_time/final.json'
+output_file = 'HFFromWikipediaWithRelation_3.json'
 with open(output_file, 'w', encoding='utf-8') as file:
-    json.dump(titles, file, indent=4, ensure_ascii=False)
+    json.dump(historical_figures, file, indent=4, ensure_ascii=False)
